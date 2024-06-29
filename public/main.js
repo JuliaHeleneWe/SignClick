bda8f97847cdc0756bf9bc253d38108cba4c14f4
JuliaHeleneW <gyros.chips@gmail.com>
2024-06-29 19:39:26 +0200

    Attempt to change designs

--- public/main.js
+++ public/main.js
@@ -6,20 +6,23 @@ const SignData = class {
 };
 
 const signMapping = {
-    'help': new SignData('Hallo, ich werde Ihnen helfen.', 'public/video1.mp4'),
+    'help': new SignData('Hallo, kann ich Ihnen helfen?', 'public/video/hallo-kann-ich-ihnen-helfen.mp4'),
     'exam': new SignData('Ich werde Sie jetzt untersuchen.', 'public/video2.mp4'),
     'hurt': new SignData('Haben Sie Schmerzen?', 'public/video1.mp4'),
-    'ambulance': new SignData('Ich rufe Ihnen jetzt einen Krankenwagen.', 'public/video1.mp4'),
+    'ambulance': new SignData('Ich werde einen Krankenwagen rufen.', 'public/video/ich-werde-einen-krankenwagen-rufen.mp4'),
 };
 
 const signContainer = document.getElementById("sign-videos");
 const player = document.getElementById('player');
 
+player.addEventListener('ended', () => {
+    let activeElement = document.getElementsByClassName('active-sign-video');
+    activeElement.classList.remove('active-sign-video');
+});
+
 const playVideo = (id, link) => {
     player.src = link;
     player.play();
-
-    document.getElementsByClassName('active-sign-video').classList.remove('active-sign-video');
     document.getElementById(id).classList.add('active-sign-video'); 
 };
 
@@ -27,8 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let mappingHtml = signContainer.innerHTML;
     for(const [index, sign] of Object.entries(signMapping)) {
         mappingHtml += (`<div id="sign-video-${index}">
-            <form id="ideas-form-${index}" method="post" onsubmit="playVideo('ideas-form-${index}','${sign.link}');return false;" class="mt-2 sign-video-btn">
-                <input type="submit" value="${sign.text}" class="text-violet-200 bg-violet-700 border-solid border-2 border-violet-700 rounded p-2" />
+            <form id="sign-form-${index}" method="post" onsubmit="playVideo('sign-button-${index}','${sign.link}');return false;" class="mt-2 sign-video-btn">
+                <input type="submit" id="sign-button-${index}" value="${sign.text}" class="text-violet-200 bg-violet-700 border-solid border-2 border-violet-700 rounded p-2" />
             </form>
         </div>`);
     };
