d3e966f380369e21dd2b0f8c23e04f439fe811f1
JuliaHeleneW <gyros.chips@gmail.com>
2024-06-30 00:32:33 +0200

    PWA test

--- public/js/main.js
+++ public/js/main.js
@@ -9,9 +9,9 @@ const signMapping = {
     'help': new SignData('Hallo, kann ich Ihnen helfen?', 'public/video/1.Satz.mp4'),
     'exam': new SignData('Ich werde Sie jetzt untersuchen.', 'public/video/6.Satz.mp4'),
     'hurt': new SignData('Haben Sie Schmerzen?', 'public/video/3.Satz.mp4'),
-    'stay': new SignData('Ich bleibe hier.', 'public/video/4.Satz.mp4'),
+    'ambulance': new SignData('Ich werde einen &lt;b&gt;Krankenwagen&lt;/b&gt; rufen.', 'public/video/2.Satz.mp4'),
     'save': new SignData('Der Rettungssanitäter ist auf halbem Weg.', 'public/video/5.Satz.mp4'),
-    'ambulance': new SignData('Ich werde einen Krankenwagen rufen.', 'public/video/2.Satz.mp4'),
+    'stay': new SignData('Ich bleibe hier.', 'public/video/4.Satz.mp4'),
     'alone': new SignData('Du bist nicht allein.', 'public/video/7.Satz.mp4'),
 };
 
@@ -32,9 +32,14 @@ const playVideo = (id, link) => {
 document.addEventListener("DOMContentLoaded", () => {
     let mappingHtml = signContainer.innerHTML;
     for(const [index, sign] of Object.entries(signMapping)) {
-        mappingHtml += (`<div id="sign-video-${index}">
+        console.log((`<div id="sign-video-${index}">
             <form id="sign-form-${index}" method="post" onsubmit="playVideo('sign-button-${index}','${sign.link}');return false;" class="mt-2 sign-video-btn">
                 <input type="submit" id="sign-button-${index}" value="${sign.text}" class="text-violet-200 bg-violet-700 border-solid border-2 border-violet-700 rounded p-2" />
+            </form>
+        </div>`));
+        mappingHtml += (`<div id="sign-video-${index}">
+            <form id="sign-form-${index}" method="post" onsubmit="playVideo('sign-button-${index}','${sign.link}');return false;" class="mt-2 sign-video-btn">
+                <input contenteditable type="submit" id="sign-button-${index}" value="${sign.text}" class="text-violet-200 bg-violet-700 border-solid border-2 border-violet-700 rounded p-2" />
             </form>
         </div>`);
     };
