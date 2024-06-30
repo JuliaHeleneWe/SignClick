35fd9a5484be84a3187afc370248554837d6b93d
Mina <mina_zarkesh@gmx.de>
2024-06-30 08:03:28 +0200

    update code

--- public/js/main.js
+++ public/js/main.js
@@ -6,13 +6,13 @@ const SignData = class {
 };
 
 const signMapping = {
-    'help': new SignData('Hallo, kann ich Ihnen helfen?', 'public/video/1.Satz.mp4'),
-    'exam': new SignData('Ich werde Sie jetzt untersuchen.', 'public/video/6.Satz.mp4'),
-    'hurt': new SignData('Haben Sie Schmerzen?', 'public/video/3.Satz.mp4'),
-    'ambulance': new SignData('Ich werde einen Krankenwagen rufen.', 'public/video/2.Satz.mp4'),
+    'help': new SignData(`Hallo, kann ich Ihnen <b>helfen</b>?`, 'public/video/1.Satz.mp4'),
+    'exam': new SignData('Ich werde Sie jetzt <b>untersuchen</b>.', 'public/video/6.Satz.mp4'),
+    'hurt': new SignData('Haben Sie <b>Schmerzen</b>?', 'public/video/3.Satz.mp4'),
+    'ambulance': new SignData('Ich werde einen <b>Krankenwagen</b> rufen.', 'public/video/2.Satz.mp4'),
     'save': new SignData('Der Rettungssanitäter ist auf halbem Weg.', 'public/video/5.Satz.mp4'),
-    'stay': new SignData('Ich bleibe hier.', 'public/video/4.Satz.mp4'),
-    'alone': new SignData('Du bist nicht allein.', 'public/video/7.Satz.mp4'),
+    'stay': new SignData('Ich <b>bleibe</b> hier.', 'public/video/4.Satz.mp4'),
+    'alone': new SignData('Du bist <b>nicht allein</b>.', 'public/video/7.Satz.mp4'),
 };
 
 const signContainer = document.getElementById("sign-videos");
@@ -47,8 +47,8 @@ document.addEventListener("DOMContentLoaded", () => {
         </div>`));
         mappingHtml += (`<div id="sign-video-${index}">
             <form id="sign-form-${index}" method="post" onsubmit="playVideo('sign-button-${index}','${sign.link}');return false;" class="mt-2 sign-video-btn">
-                <!--<button id="sign-button-${index}" class="text-violet-200 bg-violet-700 border-solid border-2 border-violet-700 rounded p-2" >${sign.text}</button>-->
-                <input contenteditable type="submit" id="sign-button-${index}" value="${sign.text}" class="text-violet-200 bg-violet-700 border-solid border-2 border-violet-700 rounded p-2" />
+                <button type="submit" id="sign-button-${index}" class="text-violet-200 bg-violet-700 border-solid border-2 border-violet-700 rounded p-2" >${sign.text}</button>
+              <!--  <input contenteditable type="submit" id="sign-button-${index}" value="${sign.text}" class="text-violet-200 bg-violet-700 border-solid border-2 border-violet-700 rounded p-2" />-->
             </form>
         </div>`);
     };
